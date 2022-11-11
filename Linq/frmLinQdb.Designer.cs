a0fd56b20226498b68c36b2a2c0bbad073c57e1e
Alex <llecopower@gmail.com>
2022-11-11 13:05:31 -0500

    Add project files.

---
+++ Linq/frmLinQdb.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Linq
+{
+    partial class frmLinQdb
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.cboCourse = new System.Windows.Forms.ComboBox();
+            this.GridResult = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GridResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(87, 35);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(102, 16);
+            this.lblCourse.TabIndex = 0;
+            this.lblCourse.Text = "Select a Course";
+            // 
+            // cboCourse
+            // 
+            this.cboCourse.FormattingEnabled = true;
+            this.cboCourse.Location = new System.Drawing.Point(227, 32);
+            this.cboCourse.Name = "cboCourse";
+            this.cboCourse.Size = new System.Drawing.Size(221, 24);
+            this.cboCourse.TabIndex = 1;
+            this.cboCourse.SelectedIndexChanged += new System.EventHandler(this.cboCourse_SelectedIndexChanged);
+            // 
+            // GridResult
+            // 
+            this.GridResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridResult.Location = new System.Drawing.Point(90, 81);
+            this.GridResult.Name = "GridResult";
+            this.GridResult.RowHeadersWidth = 51;
+            this.GridResult.RowTemplate.Height = 24;
+            this.GridResult.Size = new System.Drawing.Size(573, 312);
+            this.GridResult.TabIndex = 2;
+            // 
+            // frmLinQdb
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 449);
+            this.Controls.Add(this.GridResult);
+            this.Controls.Add(this.cboCourse);
+            this.Controls.Add(this.lblCourse);
+            this.Name = "frmLinQdb";
+            this.Text = "frmLinQdb";
+            this.Load += new System.EventHandler(this.frmLinQdb_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.ComboBox cboCourse;
+        private System.Windows.Forms.DataGridView GridResult;
+    }
+}
---
+++ Linq/frmLinQdb.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Linq
+{
+    public partial class frmLinQdb : Form
+    {
+        public frmLinQdb()
+        {
+            InitializeComponent();
+        }
+
+        //We used dataset to store many DataTables in one single collection.
+        DataSet myset = new DataSet();
+        private void frmLinQdb_Load(object sender, EventArgs e)
+        {
+            OleDbConnection myCon = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\alex\source\repos\Linq\Linq\bin\Debug\Data\School.mdb");
+            myCon.Open();
+            OleDbCommand mycmd = new OleDbCommand("SELECT * FROM Courses",myCon);
+            OleDbDataAdapter adpCourses = new OleDbDataAdapter(mycmd);
+            adpCourses.Fill(myset, "Courses");
+
+            OleDbCommand mycmd2 = new OleDbCommand("SELECT * FROM Students", myCon);
+            OleDbDataAdapter adpStudents = new OleDbDataAdapter(mycmd2);
+            adpStudents.Fill(myset, "Students");
+
+
+           // GridResult.DataSource = myset.Tables["Students"];
+
+             GridResult.DataSource = myset.Tables["Courses"];
+
+            //Fill the combobox with LINQ
+
+            var CourseTR = from DataRow cr in myset.Tables["Courses"].Rows
+                           select new
+                           {
+                               Titles = cr.Field<string>("Title"),
+                               refC = cr.Field<Int32>("RefCourse")
+                           };
+
+            cboCourse.DisplayMember = "Titles";
+            cboCourse.ValueMember = "refC";
+
+            cboCourse.DataSource = CourseTR.ToList();
+
+
+
+
+
+
+        }
+
+        private void cboCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // int refC
+        }
+    }
+}
